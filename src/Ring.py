79a3f882779cad6c10561d863786b25d301d81db
Amaar Quadri <amaarquadri@gmail.com>
2024-04-20 14:53:01 -0400

    rename to position

--- src/Ring.py
+++ src/Ring.py
@@ -4,6 +4,6 @@ import numpy as np
 
 @dataclass
 class Ring:
-    location: np.ndarray
+    position: np.ndarray
     normal: np.ndarray  # points through the ring in the desired direction of motion
     radius: float  # inner radius of the ring
