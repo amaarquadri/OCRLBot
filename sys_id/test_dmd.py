e1c66b6d06360423ca6a70215b6ff9fd795bc541
Khush Agrawal <agrawalkhush2000@gmail.com>
2024-04-20 16:55:44 -0400

    Add support for list of datas

--- sys_id/test_dmd.py
+++ sys_id/test_dmd.py
@@ -44,6 +44,37 @@ def collect_data(env,
 
     return data
 
+def create_data_matrix(data):
+    def create_individual_data_matrix(data: Dict) -> Tuple:
+        states = data['states']  # (num_samples, ndim_s)
+        actions = data['actions']  # (num_samples, ndim_a)
+
+        assert states.shape[0] == actions.shape[0]
+
+        ndim_s = states.shape[-1]
+        ndim_a = actions.shape[-1]
+
+        # Concatenate states and actions
+        x = np.hstack((states[:-1], actions[:-1])) # (num_samples - 1, ndim_s + ndim_a)
+        x_prime = states[1:] # (num_samples - 1, ndim_s)
+        info = dict(ndim_s=ndim_s, ndim_a=ndim_a)
+        return x, x_prime, info
+
+    if not isinstance(data, list):
+        data = [data]
+
+    x_list, x_prime_list = [], []
+    for d in data:
+        x, x_prime, info = create_individual_data_matrix(d)
+        x_list.append(x)
+        x_prime_list.append(x_prime)
+
+    x = np.concatenate(x_list, axis=0)
+    x_prime = np.concatenate(x_prime_list, axis=0)
+
+    return x, x_prime, info
+
+
 def perform_dmd(data):
     """Performs Dynamic Mode Decomposition on the collected data and returns A and B matrices
 
@@ -51,23 +82,15 @@ def perform_dmd(data):
     - https://www.youtube.com/watch?v=sQvrK8AGCAo&t=986s
 
     """
-    states = data['states']  # (num_samples, ndim_s)
-    actions = data['actions']  # (num_samples, ndim_a)
-
-    assert states.shape[0] == actions.shape[0]
-
-    ndim_s = states.shape[-1]
-    ndim_a = actions.shape[-1]
-
-    # Concatenate states and actions
-    x = np.hstack((states[:-1], actions[:-1])) # (num_samples - 1, ndim_s + ndim_a)
-    x_prime = states[1:] # (num_samples - 1, ndim_s)
+    x, x_prime, info = create_data_matrix(data)
+    ndim_a = info['ndim_a']
+    ndim_s = info['ndim_s']
 
     # Compute SVD of data
     U, S, Vt = svd(x.T)
 
     # Perform DMD
-    AB = np.dot(np.dot(x_prime.T, Vt[:len(S)].T), np.linalg.inv(np.diag(S))).dot(U.T)
+    AB = np.dot(np.dot(x_prime.T, Vt[:len(S)].T), np.linalg.pinv(np.diag(S))).dot(U.T)
 
     # Extract B matrix
     A = AB[:, :-ndim_a] # (ndim_s, ndim_s)
