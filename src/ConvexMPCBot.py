909ab14e1f92e33c5be948c2f5d0265474806f2c
Amaar Quadri <amaarquadri@gmail.com>
2024-04-20 15:47:28 -0400

    start working on setting up convex problem

--- src/ConvexMPCBot.py
+++ src/ConvexMPCBot.py
@@ -1,3 +1,4 @@
+import cvxpy
 import numpy as np
 import cvxpy as cp
 from OCRLBot import OCRLBot
@@ -7,6 +8,8 @@ from RingCheckpointManager import RingCheckpointManager
 
 
 class ConvexMPCBot(OCRLBot):
+    MPC_HORIZON = 240  # frames
+
     def __init__(self, name, team, index):
         super().__init__(name, team, index)
         self.A = None
@@ -15,4 +18,8 @@ class ConvexMPCBot(OCRLBot):
 
     def update(self, state: State) -> Controls:
         ring = self.checkpoint_manager.get_current_ring_checkpoint(state)
+
+        x = cp.Variable((12, ConvexMPCBot.MPC_HORIZON + 1))
+        u = cp.Variable((4, ConvexMPCBot.MPC_HORIZON))
+
         return Controls()
