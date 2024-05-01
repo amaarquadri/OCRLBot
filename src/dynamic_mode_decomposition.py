9a95e1cd5cc480e54f12550438d189dbcb198600
Amaar Quadri <amaarquadri@gmail.com>
2024-05-01 14:58:00 -0400

    use State.from_raw_state in DMD; separate collecting data, running DMD, and saving data into separate functions

--- src/dynamic_mode_decomposition.py
+++ src/dynamic_mode_decomposition.py
@@ -10,12 +10,9 @@ GLOBALS = {"np": np, "RawState": RawState, "State": State, "Controls": Controls}
 PATTERN = re.compile(r"INFO - Random Frame: RawState=(?P<raw_state>.*), State=(?P<state>.*), Controls=(?P<controls>.*)")
 
 
-def perform_dmd():
-    root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    data_dir = os.path.join(root_dir, "dmd_data")
-
-    x_kp1s: List[RawState] = []
-    x_ks: List[RawState] = []
+def parse_dmd_data(data_dir: str):
+    x_kp1s: List[State] = []
+    x_ks: List[State] = []
     u_ks: List[Controls] = []
 
     for file in os.listdir(data_dir):
@@ -23,7 +20,7 @@ def perform_dmd():
             replay_log = f.read()
 
         last_frame_number: Optional[int] = None
-        last_raw_state: Optional[RawState] = None
+        last_state: Optional[State] = None
         last_controls: Optional[Controls] = None
         for line in replay_log.split("\n"):
             match = re.match(PATTERN, line)
@@ -33,19 +30,25 @@ def perform_dmd():
             raw_state, _, controls = match.groups()
             raw_state = eval(raw_state.replace("array", "np.array"), GLOBALS)
             controls = eval(controls, GLOBALS)
+            state = State.from_raw_state(raw_state, 0)
 
             if last_frame_number is not None and raw_state.frame_number - last_frame_number == 1:
-                x_ks.append(last_raw_state)
+                x_ks.append(last_state)
                 u_ks.append(last_controls)
-                x_kp1s.append(raw_state)
+                x_kp1s.append(state)
 
-            last_frame_number = raw_state.frame_number
-            last_raw_state = raw_state
+            last_frame_number = state.frame_number
+            last_state = state
             last_controls = controls
 
-    x_kp1_matrix = np.column_stack([raw_state.to_np() for raw_state in x_kp1s])
-    x_k_matrix = np.column_stack([raw_state.to_np() for raw_state in x_ks])
-    u_k_matrix = np.column_stack([controls.to_np() for controls in u_ks])
+    print(f"Found {len(x_kp1s)} data points for DMD")
+    return x_kp1s, x_ks, u_ks
+
+
+def perform_dmd(x_kp1s: List[State], x_ks: List[State], u_ks: List[Controls]):
+    x_kp1_matrix = np.column_stack([state.to_numpy() for state in x_kp1s])
+    x_k_matrix = np.column_stack([state.to_numpy() for state in x_ks])
+    u_k_matrix = np.column_stack([controls.to_numpy() for controls in u_ks])
     M = np.row_stack([x_k_matrix,
                       u_k_matrix])
 
@@ -58,10 +61,21 @@ def perform_dmd():
     n = AB.shape[0]
     A = AB[:, :n]
     B = AB[:, n:]
+    return A, B
 
-    np.save(os.path.join(root_dir, "models", "A.npy"), A)
-    np.save(os.path.join(root_dir, "models", "B.npy"), B)
+
+def save_model(A: np.ndarray, B: np.ndarray) -> None:
+    model_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "models")
+    np.save(os.path.join(model_dir, "A.npy"), A)
+    np.save(os.path.join(model_dir, "B.npy"), B)
+
+
+def main():
+    root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    data_dir = os.path.join(root_dir, "dmd_data")
+    A, B = perform_dmd(*parse_dmd_data(data_dir))
+    save_model(A, B)
 
 
 if __name__ == '__main__':
-    perform_dmd()
+    main()
